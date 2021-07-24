d46dd08312d895bb3c42f1f2bb4de463a6dd2503
fleetyeets <tnp123@protonmail.com>
2021-07-24 14:47:45 -0400

    yee

--- setup.py
+++ setup.py
@@ -11,10 +11,11 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='weather.in',
+    name='weather_in',
     version=weather_in.__version__,
-    packages=find_packages(),
-    author='fleetyeets',
+    packages=find_packages(where='weather_in'),
+    python_requires='>=3.0',
+    author='Travis Pawlikowski',
     author_email='tnp123@protonmail.com',
     description='OpenWeather CLI script',
     long_description=long_description,
@@ -23,13 +24,14 @@ setup(
         'requests',
         'json'
     ],
-    url='https://github.com/fleetyeets/weather.in',
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/fleetyeets/weather.in/issues',
+        'Source': 'https://github.com/fleetyeets/weather.in',
+    },
     classifiers=[
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.6',
-        'Topic :: Internet :: Weather',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent'
     ]
 )
