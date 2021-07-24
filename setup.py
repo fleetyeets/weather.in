bcb11afe718eb20fed0e8954e89062cb5945b535
fleetyeets <tnp123@protonmail.com>
2021-07-24 15:43:38 -0400

    push

--- setup.py
+++ setup.py
@@ -31,4 +31,7 @@ setup(
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent'
     ],
+    entry_points = {'console_scripts':
+
+['weather_in=weather_in'],},
 )
